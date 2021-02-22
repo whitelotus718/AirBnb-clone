0206b38792968dc32902d6c3763795fbd082add2
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-02-22 11:35:31 -0500

    front end initial set up

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,19 +1,44 @@
 const router = require('express').Router();
 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+
+const { User } = require('../../db/models');
+
+
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
   
+  router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'Demo-lition'
+        },
+        })
+    setTokenCookie(res, user);
+    return res.json({ user });
+    }));
+
+  const { restoreUser } = require('../../utils/auth.js');
+  router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+      return res.json(req.user);
+    }
+  );
+  
+  
+  const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
 
 module.exports = router;
-
-
-// fetch('/api/test', {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "XSRF-TOKEN": `4v2ptztq-V10sMxzabh6a58o5QBe94pp7fsI`
-//     },
-//     body: JSON.stringify({ hello: 'world' })
-//   }).then(res => res.json()).then(data => console.log(data));
-  
